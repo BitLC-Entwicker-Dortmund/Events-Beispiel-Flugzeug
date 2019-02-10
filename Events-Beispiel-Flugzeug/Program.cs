ed952253d0acd185ebfd6f7632a693a093091476
gitenvironment <gitenvironment@web.de>
2019-02-10 16:36:48 +0100

    Add project files.

---
+++ Events-Beispiel-Flugzeug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Beispiel_Flugzeug {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
